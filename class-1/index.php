a0af505c0160d70e0bcf9870124e18fa1a0b1f9b
Md Abul Kasem <abulkasem32833@gmail.com>
2025-07-11 23:47:49 +0300

    class-1

---
+++ class-1/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP basic course</title>
+</head>
+<body style="font-size:x-large; margin:100px;  ">
+    <?php
+        
+        echo 'Hi future developer</br>';
+
+        $my_name = "Abul Kasem";
+
+        echo "$my_name </br>";
+
+
+
+        // Best Practices
+
+        // Commenting
+
+            //single line comment
+            /*
+                Multiple line comment
+             */
+
+
+        // Snake Case....... $my_name(variable/ function)
+        // Camel Case........$myName(for method)
+        // Title Case........$MyName (also called pascle case) for(class)
+
+
+        ### need to reed fig.com for standard code / best practices
+
+        $my_name = "Sagar</br>";
+
+        echo $my_name; // variable valu could be changed in run time
+
+        // contant declaration and print
+
+        define("PI", 3.1416);
+
+        echo PI;
+
+        // expression and statement
+
+        /**
+         * Data Types
+            *scaller data types
+                *Integer
+                *float/Double
+                *boolean
+                *string
+            *Compound data types
+                *Array
+                *Object
+            *Special Types
+                *Null
+                *Resource
+         */
+
+        $age = 25; //integer
+
+        $price = 30.50; //float/Double
+
+        $is_admin = false; //boolean like a question
+
+        $is_user = true; 
+
+
+        // compound Data Types
+
+        $fruits = ["Mango", "Banana", 27];
+
+        echo $fruits[1]; //use index for print any value;
+
+        $user = new User(); // here $user is an object and User is a class 
+
+    ?>
+</body>
+</html>
+
