b21c0b64bdc121a215aa6fe3c0c851f641cffd11
Md Abul Kasem <abulkasem32833@gmail.com>
2025-07-12 18:32:19 +0300

    test logical operator

--- class-2/index.php
+++ class-2/index.php
@@ -3,10 +3,53 @@
 
 //Operators
 /**
- * Addition
+ * Comparison Operator 
+ * 
  */
-$tag = "Hello Bangladesh";
+
+ $n1 = 11;
+ $n2 = "10";
+
+ $equal = $n1 == $n2;
+
+ //echo $equal;
+
+$check_with_type = $n1 === $n2;
+
+//echo $check_with_type;
 
 
-echo $tag;
+
+/*
+    var_dump($check_with_type);
+
+    $not_identical = $n1 !== $n2;
+
+    var_dump($not_identical);
+     $greater_than = $n1 > $n2;
+
+    $greater_than_or_equal = $n1 >= $n2;
+
+    var_dump($greater_than_or_equal);
+ */
+
+    $less_than_or_equal = $n1 <= $n2;
+
+    var_dump($less_than_or_equal);
+
+
+    $t = true;
+    $f = false;
+
+    $output = $t && $f;
+
+    var_dump($output);
+
+
+    var_dump($t || $f);
+
+
+
+
+
 
