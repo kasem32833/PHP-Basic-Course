4c8fc0cecf9d66a348bdb7012a8eae484fd24696
Kasem32833 <abulkasem32833@gmail.com>
2025-07-16 10:26:38 +0300

    add class-6

--- class-5/index.php
+++ class-5/index.php
@@ -1,3 +1,42 @@
 <?php
 
-echo "Hello world";
+
+// function 
+
+/*
+
+//function declaration
+
+
+function add($n1, $n2){
+    return $n1 + $n2;
+}
+
+
+// functoin calling 
+echo add(10, 20);
+
+// arguments and parameter
+// function default value
+
+
+function add($n1, $n2=30){
+    return $n1 + $n2;
+}
+
+
+
+
+
+*/
+
+function process($callback){
+    return $callback("World");
+}
+
+
+$result = process(function($name){
+    return "Hello, $name!";
+});
+
+echo $result; 
